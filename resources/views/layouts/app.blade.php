6f17fa96496d3394c257caedce08f821862897aa
neuquen8300 <dbouzo77@gmail.com>
2020-08-29 16:03:41 -0300

    seo finally

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,6 +24,7 @@
             "adressCountry" : "AR"
           },
           "geo" : {
+            "@type" : "geo",
             "latitude" : -38.9720588,
             "longitude" : -68.0474376,
           },
